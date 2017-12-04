1bc6e174d8c522fd179812657f252d5a5f667acb
jialin <450189385@qq.com>
2017-12-04 17:27:28 +0800

    modify the content

--- frontend/src/apps/home/scripts/routes/news/NewsController.js
+++ frontend/src/apps/home/scripts/routes/news/NewsController.js
@@ -16,7 +16,11 @@ define([
 
     [1, 2, 3, 4, 5].forEach(function(i) {
         data.push({
-            imgUrl: "./assets/images/news/" + i + ".png"
+            imgUrl: "./assets/images/news/" + i + ".png",
+            title: "去美国生宝宝 产后忌口",
+            desc: "月子里忌口，在民间甚为流行。有的地方至今流传着月子里除了吃小米粥和鸡蛋以外，其他什么都忌。这种忌口的习俗是不科学的。这是因为坐月子期间需要大量的营养，一是用来补充",
+            date: "2017-11-24 09:23:39",
+            viewCount: "93"
         });
     });
 
