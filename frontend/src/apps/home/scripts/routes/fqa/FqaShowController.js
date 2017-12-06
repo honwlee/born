0fd597626658467b74a216c9110454f66b570e25
jialin <450189385@qq.com>
2017-12-06 15:17:29 +0800

    modify foot and add visa,environment

--- frontend/src/apps/home/scripts/routes/fqa/FqaShowController.js
+++ frontend/src/apps/home/scripts/routes/fqa/FqaShowController.js
@@ -4,8 +4,9 @@ define([
     "handlebars",
     "simplemde",
     "server",
-    "text!scripts/routes/posts/show.hbs"
-], function($, skylarkjs, handlebars, SimpleMDE, server, showTpl) {
+    "./data",
+    "text!scripts/routes/fqa/show.hbs"
+], function($, skylarkjs, hbs, SimpleMDE, server, data, showTpl) {
     var spa = skylarkjs.spa,
         langx = skylarkjs.langx;
     return spa.RouteController.inherit({
@@ -15,26 +16,30 @@ define([
             var deferred = new langx.Deferred(),
                 self = this,
                 id = e.route.getNamedValue()[1];
-            server().connect("posts", "get", "show?id=" + id).then(function(post) {
-                self.post = post;
-                deferred.resolve();
-            });
-            e.result = deferred.promise;
+            // server().connect("posts", "get", "show?id=" + id).then(function(post) {
+            //     self.post = post;
+            //     deferred.resolve();
+            // });
+            // e.result = deferred.promise;
+            self.post = data.filter(function(d) {
+                return d.id == id;
+            })[0];
+            e.result = langx.Deferred.when(true);
         },
         rendering: function(e) {
             var selector = $(langx.trim(showTpl));
-            var tpl = handlebars.compile(langx.trim(selector.find("#post-show-main").html()).replace("{{&gt;", "{{>"));
+            var tpl = hbs.compile(langx.trim(selector.find("#fqa-show-main").html()).replace("{{&gt;", "{{>"));
 
             e.content = $(tpl({
                 id: this.post.id,
                 title: this.post.title,
-                tags: this.post.tags || "",
-                date: new Date(this.post.updatedAt).toISOString().substring(0, 10)
+                imgUrl: this.post.imgUrl,
+                date: new Date(this.post.date).toISOString().substring(0, 10)
             }));
             var simplemde = new SimpleMDE({
                 element: e.content.find("textarea")[0]
             });
-            e.content.find(".post_overview").html(simplemde.markdown(this.post.markdown));
+            e.content.find(".post_overview").html(simplemde.markdown(this.post.content));
             simplemde.toTextArea();
             simplemde = null;
         },
